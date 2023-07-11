14e55eea8756bdf34e542536a44eddd459dfed88
luoyutong <luoyutong@lizhi.fm>
2023-07-11 17:03:10 +0800

    Initial Commit

---
+++ ex3_delStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ex3_delStoryboard
+//
+//  Created by yutong on 2023/7/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
